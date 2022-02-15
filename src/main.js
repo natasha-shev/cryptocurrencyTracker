13f74784039a520d0d62303b073ad47bd53f0999
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-15 16:16:10 +0300

    create store

--- src/main.js
+++ src/main.js
@@ -1,14 +1,38 @@
-import { createApp } from 'vue'
-//import Vue from 'vue'
+import {createApp} from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 
+const store = createStore({
+    state: {
+        status: '',
+        token: localStorage.getItem('token'), //|| '',
+        user : {}
+    },
+
+    mutations: {
+        auth_success(state, token){
+            state.status = 'success'
+            state.token = token
+            //state.user = user //мой бэк не возвращает юзера (пока что)
+        },
+        auth_error(state){
+            state.status = 'error'
+        },
+    },
+
+    getters: {
+        isLoggedIn: state => !!state.token, //!!null == false
+        //authStatus: state => state.status, //разобраться это вообще зачем у них в примере былооо
+    },
+
+
+});
 
 const app = createApp(App);
+
 app.use(router);
+app.use(store);
 app.mount('#app');
-//app.$router = router;
-// new Vue({
-//     router,
-//     render: h => h(App),
-// }).$mount('#app')
+
+export default store;
