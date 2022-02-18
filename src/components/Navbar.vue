0faf4fd3fa29b6f9e049a29cf5eb1ae050b0b2e4
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-18 12:32:30 +0300

    replace AppTitle wish Navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <strong>Cryptocurrency App</strong>
+    </div>
+
+    <!--      <v-spacer></v-spacer>-->
+
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style scoped>
+
+</style>
