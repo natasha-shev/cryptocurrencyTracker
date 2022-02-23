57393e12ed9983be0b994ec8c02a01526461e52f
Natalia Shevtsova <localcatgod@gmail.com>
2022-02-23 15:24:16 +0300

    add addToFavourites methods

--- src/components/CurrencyName.vue
+++ src/components/CurrencyName.vue
@@ -3,9 +3,9 @@
     <img :src="curr.icon_url">
     <strong id="names">{{ curr.name }} | {{ curr.full_name }}</strong>
     <v-spacer />
-    <v-btn
-      icon
-      color="#fcad03"
+    <v-btn icon
+           :color="favBtnColor"
+           @click="addToFavourites"
     >
       <v-icon>mdi-star</v-icon>
     </v-btn>
@@ -13,9 +13,29 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'CurrencyName',
-  props: ['curr'] // Add to favourites
+  props: ['curr'],
+  data() {
+    return {
+      favBtnColor: 'white' // statement if....
+    };
+  },
+  methods: {
+    addToFavourites() {
+      this.favBtnColor = this.favBtnColor === 'white' ? '#fcad03' : 'white';
+      axios
+        .post('http://127.0.0.1:8000/api/favourite/' + this.curr.id, {}, {
+          headers: {
+            'Authorization': `Bearer ${localStorage.getItem('token')}`
+          }
+        }).then(response => {
+        console.log(response.data);
+      });
+    }
+  }
 };
 </script>
 
