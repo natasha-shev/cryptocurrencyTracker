ced19ae4533d39b05a73a3de4cb8c98f96b75fe4
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-07 23:26:09 +0300

    fix parseDate

--- src/components/AddCoinDialog.vue
+++ src/components/AddCoinDialog.vue
@@ -163,7 +163,7 @@ export default {
     parseDate(date) {
       if (!date) { return null; }
 
-      const [month, day, year] = date.split('/');
+      const [day, month, year] = date.split('/');
 
       return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
     },
