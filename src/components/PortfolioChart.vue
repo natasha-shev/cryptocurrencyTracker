3c76b878011e7a4c5d6d4424e46f96f55d34d7b2
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-21 13:35:27 +0300

    add total data to chart

--- src/components/PortfolioChart.vue
+++ src/components/PortfolioChart.vue
@@ -1,24 +1,53 @@
 <template>
-  <JSCharting :options="chartOptions" class="columnChart"></JSCharting>
+  <JSCharting
+    v-if="loaded"
+    :options="chartOptions"
+    class="columnChart"
+  ></JSCharting>
+  <div v-else class="text-center">
+    <v-progress-circular
+      indeterminate
+      color="primary"
+    ></v-progress-circular>
+  </div>
 </template>
 
 <script>
 import JSCharting from 'jscharting-vue';
+import api from '../api';
 
 export default {
   name: 'PortfolioChart',
   data() {
     return {
+      loaded: false,
       chartOptions: {
+        legend: {
+          template: '%icon %name',
+          position: 'inside top left'
+        },
+        defaultPoint_tooltip:
+          'Date <b>%xValue</b> <br/>' +
+          'Total <b>%yValue</b>',
         defaultPoint_marker_type: 'none',
+        xAxis: {
+          crosshair_enabled: true,
+          label_text: "Date",
+        },
+        yAxis: {
+          label_text: "Total",
+          formatString: 'c',
+          crosshair: {
+            enabled: true,
+            snapToData: true,
+          },
+        },
+        debug: true,
         type: 'line',
         series: [
           {
-            points: [
-              { x: 1, y: 50 },
-              { x: 8, y: 30 },
-              { x: 10, y: 50 }
-            ]
+            name: 'Portfolio',
+            points: []
           }
         ]
       }
@@ -28,8 +57,14 @@ export default {
     JSCharting
   },
 
-  methods: {
-
+  beforeCreate() {
+    api.getTotal().then(response => {
+      this.chartOptions.series[0].points = response.data.map((value) => {
+        return [new Date(value.datetime), value.total];
+      });
+      this.loaded = true;
+      //console.log(new Date(response.data[0].datetime));
+    });
   }
 };
 </script>
