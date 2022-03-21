6bc4508fd516a29b4597ffa36e813157ef7f698c
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-21 17:06:58 +0300

    add SalesDialog

---
+++ src/components/SalesDialog.vue
@@ -0,0 +1,29 @@
+<template>
+<div>Oh shit here we go again</div>
+</template>
+
+<script>
+import api from '../api';
+
+export default {
+  name: 'SalesDialog',
+
+  data() {
+    return {
+      loaded: false,
+
+    };
+  },
+
+  beforeCreate() {
+    api.getSales().then(response => {
+      this.loaded = true;
+      console.log(response.data);
+    });
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -18,11 +18,11 @@
 
     <v-dialog
       v-model="portfolioDialog"
-      max-width="800"
+      max-width="900"
     >
       <v-card>
         <v-card-title class="text-h5">
-          Use Google's location service?
+          Natasha didn't come up with the text
         </v-card-title>
 
         <v-card-text>
@@ -39,7 +39,39 @@
           </v-btn>
         </v-card-actions>
       </v-card>
+    </v-dialog>
 
+    <v-btn
+      tile
+      depressed
+      @click.stop="salesDialog = true"
+    >
+      Sales
+    </v-btn>
+
+    <v-dialog
+      v-model="salesDialog"
+      max-width="900"
+    >
+      <v-card>
+        <v-card-title class="text-h5">
+          Natasha didn't come up with the text again
+        </v-card-title>
+
+        <v-card-text>
+          <SalesDialog></SalesDialog>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            tile
+            @click="salesDialog = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
     </v-dialog>
 
     <CurrenciesList class="mt-6"
@@ -53,14 +85,16 @@ import CurrenciesList from '../components/CurrenciesList';
 import store from '../store';
 import AddCoinDialog from '../components/AddCoinDialog';
 import PortfolioChart from '../components/PortfolioChart';
+import SalesDialog from '../components/SalesDialog';
 export default {
   name: 'Favourites',
-  components: { PortfolioChart, AddCoinDialog, CurrenciesList },
+  components: { SalesDialog, PortfolioChart, AddCoinDialog, CurrenciesList },
 
   data() {
     return {
       addDialog: false,
-      portfolioDialog: false
+      portfolioDialog: false,
+      salesDialog: false
     };
   },
 
