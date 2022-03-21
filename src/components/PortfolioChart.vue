2039692d7825704b12c9c9b6a37a9eed73b47fef
Natalia Shevtsova <localcatgod@gmail.com>
2022-03-21 11:55:44 +0300

    add PortfolioChart component

---
+++ src/components/PortfolioChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <JSCharting :options="chartOptions" class="columnChart"></JSCharting>
+</template>
+
+<script>
+import JSCharting from 'jscharting-vue';
+
+export default {
+  name: 'PortfolioChart',
+  data() {
+    return {
+      chartOptions: {
+        defaultPoint_marker_type: 'none',
+        type: 'line',
+        series: [
+          {
+            points: [
+              { x: 1, y: 50 },
+              { x: 8, y: 30 },
+              { x: 10, y: 50 }
+            ]
+          }
+        ]
+      }
+    };
+  },
+  components: {
+    JSCharting
+  },
+
+  methods: {
+
+  }
+};
+</script>
+
+<style>
+.columnChart {
+  height: 300px;
+}
+</style>
